785cc1d421dadfafb639a3ef4ea448fe0d05720f
Peter Maguire <peter@unacceptableuse.com>
2016-02-14 14:34:14 +0000

    Refactored local variables

--- js/store.js
+++ js/store.js
@@ -4,7 +4,9 @@
 /**
  * Created by Peter on 05/02/2016.
  */
-Ethan.Store = function(game){};
+Ethan.Store = function(game){
+
+};
 Ethan.Store.prototype = {
 
     create: function(){
@@ -12,24 +14,24 @@ Ethan.Store.prototype = {
             window.game.unlockAchievement("CgkIrP6I1bcCEAIQBA");
         }
         if (this.game.senpaiMode) {
-            background = this.game.add.group();
+            Ethan.Store.background = this.game.add.group();
             for (var i = 0; i < 20; i++) {
                 var arrow = this.game.add.sprite(this.game.rnd.integerInRange(0, 1080), this.game.rnd.integerInRange(0, 1920), "background");
                 var scale = this.game.rnd.integerInRange(1, 100) / 100;
                 arrow.scale.setTo(scale, scale);
                 arrow.speed = this.game.rnd.integerInRange(1, 10);
-                background.add(arrow);
+                Ethan.Store.background.add(arrow);
             }
         } else {
-            background = this.add.image(0, 0, "space");
-            background.anchor.setTo(0.5, 0.5);
-            background.angle = 90;
-            background.scale.setTo(2.5, 2);
+            Ethan.Store.background = this.add.image(0, 0, "space");
+            Ethan.Store.background.anchor.setTo(0.5, 0.5);
+            Ethan.Store.background.angle = 90;
+            Ethan.Store.background.scale.setTo(2.5, 2);
         }
 
-       ownedPowerups = JSON.parse(localStorage.getItem("powerups")) || {};
-        storefront = this.add.image(50, 200, "storefront");
-        storeHeader = this.add.image(230, 50, "storeheader");
+        Ethan.Store.ownedPowerups = JSON.parse(localStorage.getItem("powerups")) || {};
+        Ethan.Store.storeFront = this.add.image(50, 200, "storefront");
+        Ethan.Store.storeHeader = this.add.image(230, 50, "storeheader");
 
         var costStyle = {
             font: "34px Arial",
@@ -72,7 +74,7 @@ Ethan.Store.prototype = {
             bmp.draw(this.game.make.text(200,0, powerup.name, titleStyle));
             bmp.draw(this.game.make.text(200,80, powerup.desc, descStyle));
             bmp.draw(this.game.make.text(770, 150, powerup.cost, costStyle));
-            if(ownedPowerups[powerup.bool]){
+            if(Ethan.Store.ownedPowerups[powerup.bool]){
                 sellCount ++;
                 bmp.draw(sellText);
             }
@@ -86,17 +88,17 @@ Ethan.Store.prototype = {
             }
         }
 
-        balance = this.add.text(160,210, "x"+this.game.ethanBucks, {
+        Ethan.Store.balanceText = this.add.text(160,210, "x"+this.game.ethanBucks, {
             font: "48px Arial",
             fill: "#000000"
         });
 
-        backButton = this.game.add.button(240, 1750, 'back', this.mainMenu, this, 1, 1, 1);
+        Ethan.Store.backButton = this.game.add.button(240, 1750, 'back', this.mainMenu, this, 1, 1, 1);
     },
 
     update: function(){
         if(this.game.senpaiMode){
-            background.forEach(function(arrow){
+            Ethan.Store.background.forEach(function(arrow){
                 arrow.x-=arrow.speed;
                 if(arrow.x < -300){
                     arrow.x = 1080;
@@ -108,37 +110,37 @@ Ethan.Store.prototype = {
     buyItem: function(button){
         var item = powerups[button.purchase];
 
-        if(!ownedPowerups[item.bool]){
-            if(item.cost > this.game.ethanBucks){
-                this.game.messageBox("You don't have enough EthanBucks to purchase this item!\n\nWould you like to go to the EthanBucks store now?", function(){
-                    this.game.state.start("EthanBucks");
-                },function(){
-                    this.game.state.start("Store");
-                }, this);
-            }else{
-                this.game.ethanBucks -= item.cost;
-                localStorage.setItem("ethanbucks", parseInt(this.game.ethanBucks));
-                ownedPowerups[item.bool] = true;
-                localStorage.setItem("powerups", JSON.stringify(ownedPowerups));
-                if(item.bool === "senpai"){
-                    menumusic.stop();
-                    this.game.state.start("Boot");
-                }else{
-                    this.game.state.start("Store");
-                }
-            }
-        }else{
+        if (Ethan.Store.ownedPowerups[item.bool]) {
             this.game.ethanBucks = parseInt(this.game.ethanBucks) + parseInt(item.cost);
-            ownedPowerups[item.bool] = false;
+            Ethan.Store.ownedPowerups[item.bool] = false;
             localStorage.setItem("ethanbucks", parseInt(this.game.ethanBucks));
-            localStorage.setItem("powerups", JSON.stringify(ownedPowerups));
-            if(item.bool === "senpai"){
+            localStorage.setItem("powerups", JSON.stringify(Ethan.Store.ownedPowerups));
+            if (item.bool === "senpai") {
                 menumusic.stop();
                 this.game.state.start("Boot");
-            }else{
+            } else {
                 this.game.state.start("Store");
             }
 
+        } else {
+            if (item.cost > this.game.ethanBucks) {
+                this.game.messageBox("You don't have enough EthanBucks to purchase this item!\n\nWould you like to go to the EthanBucks store now?", function () {
+                    this.game.state.start("EthanBucks");
+                }, function () {
+                    this.game.state.start("Store");
+                }, this);
+            } else {
+                this.game.ethanBucks -= item.cost;
+                localStorage.setItem("ethanbucks", parseInt(this.game.ethanBucks));
+                Ethan.Store.ownedPowerups[item.bool] = true;
+                localStorage.setItem("powerups", JSON.stringify(Ethan.Store.ownedPowerups));
+                if (item.bool === "senpai") {
+                    menumusic.stop();
+                    this.game.state.start("Boot");
+                } else {
+                    this.game.state.start("Store");
+                }
+            }
         }
     },
 
